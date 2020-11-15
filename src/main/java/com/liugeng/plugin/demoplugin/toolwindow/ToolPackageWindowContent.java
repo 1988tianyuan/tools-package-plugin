89a5f59393200cdca01f2bd454c41dd3669ff728
1988tianyuan <467651794@qq.com>
2020-11-15 18:09:37 +0800

    add editor panel

--- src/main/java/com/liugeng/plugin/demoplugin/toolwindow/ToolPackageWindowContent.java
+++ src/main/java/com/liugeng/plugin/demoplugin/toolwindow/ToolPackageWindowContent.java
@@ -1,19 +1,69 @@
 package com.liugeng.plugin.demoplugin.toolwindow;
 
+import com.google.gson.*;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.EditorSettings;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.psi.PsiFile;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ToolPackageWindowContent {
 
     private JPanel panelContent;
-    private JLabel demoLabel;
-    private JButton demoButton;
+    private JButton parseButton;
+    private JTextPane output;
+    private JPanel inputContainer;
+    private JPanel outputContainer;
+    private JPanel input;
+    private final Gson gson;
+    private final Project project;
 
-    public ToolPackageWindowContent(ToolWindow toolWindow) {
-        demoButton.addActionListener(e -> {
-            demoLabel.setText("hahaha");
+    public ToolPackageWindowContent(ToolWindow toolWindow, Project project) {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        this.project = project;
+        Editor editor = createEditor();
+        input.setAutoscrolls(true);
+        input.add(editor.getComponent(), BorderLayout.CENTER);
+        parseButton.addActionListener(e -> {
+            String inputStr = editor.getDocument().getText();
+            String outputStr = parseInputJsonStr(inputStr);
+            output.setText(outputStr);
         });
+    }
+
+    private Editor createEditor() {
+        PsiFile myFile = null;
+        EditorFactory editorFactory = EditorFactory.getInstance();
+        Document doc = editorFactory.createDocument("");
+        Editor editor = editorFactory.createEditor(doc, project);
+        EditorSettings editorSettings = editor.getSettings();
+        editorSettings.setVirtualSpace(false);
+        editorSettings.setLineMarkerAreaShown(false);
+        editorSettings.setIndentGuidesShown(false);
+        editorSettings.setFoldingOutlineShown(true);
+        editorSettings.setAdditionalColumnsCount(3);
+        editorSettings.setAdditionalLinesCount(6);
+        editorSettings.setLineNumbersShown(true);
+        editorSettings.setCaretRowShown(true);
+        return editor;
+    }
+
+    private String parseInputJsonStr(String jsonStr) {
+        String result;
+        try {
+            JsonElement jsonElement = JsonParser.parseString(jsonStr);
+            result = gson.toJson(jsonElement);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+            String errorMessage = e.getCause().getMessage();
+            result = "Invalid json string: " + jsonStr + "\n err: " + errorMessage;
+        }
+        return result;
     }
 
     public JPanel getPanelContent() {
--- src/main/java/com/liugeng/plugin/demoplugin/toolwindow/ToolPackagesWindowFactory.java
+++ src/main/java/com/liugeng/plugin/demoplugin/toolwindow/ToolPackagesWindowFactory.java
@@ -12,7 +12,7 @@ public class ToolPackagesWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ToolPackageWindow toolPackageWindow = new ToolPackageWindow();
-        ToolPackageWindowContent toolPackageWindowContent = new ToolPackageWindowContent(toolWindow);
+        ToolPackageWindowContent toolPackageWindowContent = new ToolPackageWindowContent(toolWindow, project);
         toolPackageWindow.setContent(toolPackageWindowContent.getPanelContent());
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(toolPackageWindow, "",
